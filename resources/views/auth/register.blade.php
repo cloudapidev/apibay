e56e08ad2a2cd8c33ab4f13b6348f28ebc34e55e
wy <wy@wy.com>
2016-01-21 15:38:54 +0800

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,11 +52,11 @@
 					<input type="hidden" name="failregister" value="1">
 					<input type="hidden" name="_token"         value="{{csrf_token()}}"/>
           <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Full name" name='full_name' value='{{old("full_name")}}'>
+            <input type="text" class="form-control" placeholder="Full name" name='full_name' '>
             <span class="glyphicon glyphicon-user form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="email" class="form-control" placeholder="Email" name='email' value='{{old("email")}}' >
+            <input type="email" class="form-control" placeholder="Email" name='email' ' >
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
